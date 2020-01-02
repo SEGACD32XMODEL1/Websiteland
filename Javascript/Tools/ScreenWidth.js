5116af9c9d62fc5bc7c526251b6e7de185c6ee54
SEGACD32XMODEL1 <33665206+SEGACD32XMODEL1@users.noreply.github.com>
2020-01-02 09:06:43 -0600

    Mobile Redirect and Sizing Testing

---
+++ Javascript/Tools/ScreenWidth.js
@@ -0,0 +1,3 @@
+if (screen.width <= 750) {
+document.location = "https://SEGACD32XMODEL1.github.io/Websiteland/mobile.html";
+}
---
+++ Javascript/Tools/UserAgent.js
@@ -0,0 +1,2 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)){
+window.location ="https://SEGACD32XMODEL1.github.io/Websiteland/mobile.html";}
